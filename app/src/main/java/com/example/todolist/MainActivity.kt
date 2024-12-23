4ad41c39c9ce007b16a229a7ea2ff506462c7f95
markomitrovicc <mitrovicmarkoit@gmail.com>
2024-12-23 22:49:40 +0100

    Uradjen add text i LazyColumn sa itemima. Text unutar outlinedTextField-a se ne vidi iz nekog razloga.

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,44 +4,130 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.todolist.ui.theme.TodoListTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TodoListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+            var item by remember {
+                mutableStateOf("")
             }
+
+            var listOfItems by remember {
+                mutableStateOf(listOf<String>())
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .systemBarsPadding()
+                    .padding(horizontal = 10.dp,)
+                    .padding(top = 10.dp)
+            ) {
+
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    OutlinedTextField(
+                        value = item,
+                        textStyle = TextStyle(color = Color.Black, fontSize = 30.sp),
+                        onValueChange = {  text ->
+                            item = text
+                        },
+                        modifier = Modifier
+                            .height(35.dp)
+                    )
+
+                    Button(modifier = Modifier
+                                .height(35.dp)
+                                .padding(start = 5.dp),
+                        onClick = {
+                        if (item.isNotBlank() && !listOfItems.contains(item)){
+                            listOfItems = listOfItems + item
+                        }
+                    }) {
+                        Text("Add")
+                    }
+
+                }
+
+                LazyColumn {
+                    items(listOfItems) { currentItem ->
+                        Column (
+                            modifier = Modifier.padding(vertical = 10.dp)
+                        ){
+                            ItemRow(currentItem)
+                        }
+                    }
+                }
+
+            }
+
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun ItemRow(text: String) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                width = 2.dp,
+                colorResource(R.color.purple_500),
+                shape = RoundedCornerShape(5.dp)
+                )
+            .padding(horizontal = 16.dp)
+    ) {
+        Text(
+            text,
+            modifier = Modifier
+                .fillMaxWidth(),
+            style = TextStyle(fontSize = 30.sp)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TodoListTheme {
-        Greeting("Android")
+        )
     }
 }
+
